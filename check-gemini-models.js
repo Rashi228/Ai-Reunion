fecb24601d3fc258b69485eb3c9290c072ffd29f
Rashi228 <guptarashi2201@gmail.com>
2025-08-19 21:45:52 +0530

    Update check-gemini-models.js

--- check-gemini-models.js
+++ check-gemini-models.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const GEMINI_API_KEY = 'AIzaSyCTjbLbDT89Aa0M6KeBjgHShCKJLMaasx4'; // Use your Gemini API key here
+
 
 axios.get('https://generativelanguage.googleapis.com/v1/models?key=' + GEMINI_API_KEY)
   .then(res => console.log(res.data))
